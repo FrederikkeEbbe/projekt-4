46174ea610f4e407ee252881fcc29a306d4ba5b2
FrederikkeEbbe <febbe8@gmail.com>
2023-03-26 22:51:30 +0200

    Burger menu
    Uden links der virker og minimal styling

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+//Frede
+window.onload = function () {
+	const menu_btn = document.querySelector('.hbmenu');
+	const mobil_menu = document.querySelector('.mobil-nav');
 
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('aktivnu');
+		mobil_menu.classList.toggle('aktivnu');
+	});
+}
+
+//Asger
+
+//Jacob
+
+//Jasmin
