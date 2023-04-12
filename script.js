3253754487a0bc0b25ec89ad9da31dc6620592fb
FrederikkeEbbe <febbe8@gmail.com>
2023-04-12 15:44:12 +0200

    Login mulighed
    Tilføjet JS, CSS og HTML. Virker ikke helt endnu, men er tæt på.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//Frede
+//Frede 
+//Burgermenu
 window.onload = function () {
 	const menu_btn = document.querySelector('.hbmenu');
 	const mobil_menu = document.querySelector('.mobil-nav');
@@ -8,6 +9,24 @@ window.onload = function () {
 		mobil_menu.classList.toggle('aktivnu');
 	});
 }
+
+//Login/registrer
+const loginWrap = document.querySelector('.loginwrap');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopOp = document.querySelector('ikon-luk')
+
+registerLink.addEventListener('click', ()=> {
+	loginWrap.classList.add('active');
+})
+
+loginLink.addEventListener('click', ()=> {
+	loginWrap.classList.remove('active');
+})
+
+btnPopOp.addEventListener('click', ()=> {
+	loginWrap.classList.add('active-popup');
+})
 
 //Asger
 
