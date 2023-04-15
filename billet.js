cdaf2b18a1852a37dee0c5d5af6bb082ed19b896
jasm1679 <jdch48625@edu.ucl.dk>
2023-04-15 22:29:24 +0200

    Billetter

--- billet.js
+++ billet.js
@@ -1,6 +1,22 @@
 var interval = null;
-function flyv() {
-    var elem = document.getElementById("animationbillet");   
+function flyv1() {
+    var elem = document.getElementById("animationbillet1");   
+    var position = -35;
+    clearInterval(interval);
+    interval = setInterval(frame, 5);
+    function frame() {
+            if (position == 300) {
+                clearInterval(interval);         
+            } else {
+                position++; 
+                elem.style.top = position + 'px'; 
+            }
+    }
+}
+
+var interval = null;
+function flyv2() {
+    var elem = document.getElementById("animationbillet2");   
     var position = -35;
     clearInterval(interval);
     interval = setInterval(frame, 5);
