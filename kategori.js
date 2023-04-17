440bb2e1057d1ec2930f27b3b49187c086967850
xthrane <asger@heuck.dk>
2023-04-17 09:21:11 +0200

    ændret kommentarer i js

--- kategori.js
+++ kategori.js
@@ -1,10 +1,10 @@
-// ----- Sprogvælger -----
+// ----- Kategorivælger -----
 
 select_element = document.getElementById("kategoriVælger");
 let kategori = ["Kategori", "Sport", "Fest", "Foredrag", "Karriere", "Hobby",];
 
 
-// Udvider option-boks baseret på antal sprog i array'et og sætter første sprog i array'et som det valgte
+// Udvider option-boks baseret på antal kategorier i array'et og sætter første input i array'et som det valgte
 for (let i = 0; i < kategori.length; i++) {
     let opt = kategori[i];
     if (i == 0)
