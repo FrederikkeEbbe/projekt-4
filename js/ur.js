aff2f8d2d6e36fed1ffd3e820deb0fc6b62f26f2
FrederikkeEbbe <febbe8@gmail.com>
2023-04-20 22:53:16 +0200

    ændring af var til let JS

--- js/ur.js
+++ js/ur.js
@@ -1,21 +1,21 @@
 // Frederikke count down
 //tilføj en var med en dato. Den gemmer tid og dato, når nedtællingen ender
-var countDownDato = new Date("Sep 15, 2023 00:00:00").getTime();
+let countDownDato = new Date("Sep 15, 2023 00:00:00").getTime();
 
 //Tilføjer en funktion. Den gentager sig ved 1000 milisekunder. Det vil sige et sekundt
-var x = setInterval(function(){
+let x = setInterval(function(){
 
   //Tølføjer i dag og tiden
-  var now = new Date().getTime();
+  let now = new Date().getTime();
   // Tilføjer afstanden mellem i dags dato, og count downs slut dato.
-  var distance = countDownDato - now;
+  let distance = countDownDato - now;
 
   // Nu udregnes afstanden på dage, timer, minutter og sekunder.
   // Her gemmer vi det i flere variabler. 
-  var dage = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var timer = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutter = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var sekunder = Math.floor((distance % (1000 * 60)) / 1000);
+  let dage = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let timer = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutter = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let sekunder = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Nu skal vi fremvise/display dette 
   document.getElementById("dage").innerHTML = dage;
